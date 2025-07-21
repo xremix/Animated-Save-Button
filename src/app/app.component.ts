a251b092501b892ea3f5f7f815de03d160208246
Toni Hoffmann <toni.hoffmann@arthrex.de>
2025-07-21 08:20:26 +0200

    adding a first example form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import "@awesome.me/webawesome/dist/components/button/button.js"
 import "@awesome.me/webawesome/dist/components/icon/icon.js"
+import "@awesome.me/webawesome/dist/components/input/input.js";
+import "@awesome.me/webawesome/dist/components/select/select.js";
+import "@awesome.me/webawesome/dist/components/textarea/textarea.js";
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
