1b89ae54db460b5009c2b9249ade218ef9071847
xremix <tonidhoffmann@gmail.com>
2025-07-26 12:41:27 +0200

    button blue

--- animated-save-button.js
+++ animated-save-button.js
@@ -4,9 +4,9 @@ class AnimatedSaveButton extends LitElement {
   static styles = css`
     button {
       padding: 8px 16px;
-      border: 1px solid #ccc;
-      background-color: #f8f9fa;
-      color: #333;
+      border: 1px solid rgb(3, 98, 193);
+      background-color:rgb(0, 112, 225);
+      color:rgb(255, 255, 255);
       font-size: 14px;
       cursor: pointer;
       border-radius: 4px;
